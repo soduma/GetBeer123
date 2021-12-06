24c34f7f01ae9d0f12b7b03fa68f9f500e96466d
soduma <soduma@naver.com>
2021-12-06 15:53:12 +0900

    page prefetch

--- GetBeer/BeerListTableViewController.swift
+++ GetBeer/BeerListTableViewController.swift
@@ -11,6 +11,7 @@ class BeerListTableViewController: UITableViewController {
 
     var beerList = [Beer]()
     var currentPage = 1
+    var dataTasks = [URLSessionTask]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +23,12 @@ class BeerListTableViewController: UITableViewController {
         //UITableView
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
-        
+        tableView.prefetchDataSource = self
         fetchBeer(of: currentPage)
     }
 }
 
-extension BeerListTableViewController {
+extension BeerListTableViewController: UITableViewDataSourcePrefetching {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
     }
@@ -36,6 +37,7 @@ extension BeerListTableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
         let beer = beerList[indexPath.row]
         cell.configure(with: beer)
+//        print("Rows: \(indexPath.row)")
         return cell
     }
     
@@ -45,12 +47,25 @@ extension BeerListTableViewController {
         detailViewController.beer = selectedBeer
         show(detailViewController, sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+//        indexPaths.forEach {
+//            print("prefetchRows: \($0.row)")
+//        }
+        guard currentPage != 1 else { return }
+        indexPaths.forEach {
+            if ($0.row + 1) / 25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
 }
 
 //data fetching
 private extension BeerListTableViewController {
     func fetchBeer(of page: Int) {
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+        dataTasks.firstIndex(where: { $0.originalRequest?.url == url }) == nil else { return } // datatask 중복방지
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -82,5 +97,6 @@ private extension BeerListTableViewController {
             }
         }
         dataTask.resume()
+        dataTasks.append(dataTask)
     }
 }
